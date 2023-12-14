0874fc92d8c6b978c81788c0729e15551572a3e9
Bryan Astacio <brya0196@gmail.com>
2023-12-13 21:30:04 -0400

    feat: include the webpack remover on ext

--- packages/extension/craco.config.js
+++ packages/extension/craco.config.js
@@ -3,6 +3,7 @@ const { getWebpackTools } = require("react-native-monorepo-tools");
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 const monorepoWebpackTools = getWebpackTools();
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
 module.exports = {
   devServer: {
@@ -16,6 +17,8 @@ module.exports = {
       monorepoWebpackTools.enableWorkspacesResolution(webpackConfig);
       // Ensure nohoisted libraries are resolved from this workspace.
       monorepoWebpackTools.addNohoistAliases(webpackConfig);
+
+      webpackConfig.resolve.plugins = webpackConfig.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
       return webpackConfig;
     },
     plugins: [
